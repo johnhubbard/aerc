johnhubbard/aerc

commit 741f93d991bbc01f41ef00095d05e3530cc34884

ui: allow centering the message viewer and composer
On wide screens, it is common to have aerc running in a wide window. In
that case, the message viewer and the composer aligned to the left leave
the user looking at the side of the window for long periods of time,
which can be tiresome if the window sits on a side of the screen.
Introduce an option to allow the message viewer and the composer to be
centered on the window by setting a fixed width for them and padding
both sides with empty space.

Changelog-added: The editor and the message viewer can now be centered
 on screen using a fixed width.
Signed-off-by: inwit <inwit@sindominio.net>
Tested-by: Antonin Godard <antonin@godard.cc>
Acked-by: Robin Jarry <robin@jarry.cc>

app/compose.go
@@ -45,19 +45,20 @@ type Composer struct {
 	acct       *AccountView
 	seldir     string
 
-	attachments []lib.Attachment
-	editor      *Terminal
-	email       *os.File
-	grid        atomic.Value
-	heditors    atomic.Value // from, to, cc display a user can jump to
-	review      *reviewMessage
-	worker      *types.Worker
-	completer   *completer.Completer
-	crypto      *cryptoStatus
-	sign        bool
-	encrypt     bool
-	attachKey   bool
-	editHeaders bool
+	attachments         []lib.Attachment
+	editor              *Terminal
+	email               *os.File
+	grid                atomic.Value
+	heditors            atomic.Value // from, to, cc display a user can jump to
+	review              *reviewMessage
+	worker              *types.Worker
+	completer           *completer.Completer
+	crypto              *cryptoStatus
+	sign                bool
+	encrypt             bool
+	attachKey           bool
+	editHeaders         bool
+	centeredLayoutWidth int
 
 	layout    HeaderLayout
 	focusable []ui.MouseableDrawableInteractive
@@ -106,7 +107,8 @@ func NewComposer(
 		focused:   1,
 		completer: nil,
 
-		editHeaders: editHeaders,
+		editHeaders:         editHeaders,
+		centeredLayoutWidth: acct.UiConfig().CenteredLayoutWidth,
 	}
 
 	data := state.NewDataSetter()
@@ -1199,10 +1201,14 @@ func (c *Composer) DeleteAttachment(name string) error {
 }
 
 func (c *Composer) resetReview() {
+	var mainCol int = 0
+	if c.centeredLayoutWidth != 0 {
+		mainCol = 1
+	}
 	if c.review != nil {
 		c.grid.Load().(*ui.Grid).RemoveChild(c.review)
 		c.review = newReviewMessage(c, nil)
-		c.grid.Load().(*ui.Grid).AddChild(c.review).At(3, 0)
+		c.grid.Load().(*ui.Grid).AddChild(c.review).At(3, mainCol)
 	}
 }
 
@@ -1502,9 +1508,20 @@ func (c *Composer) delEditor(header string) {
 
 // updateGrid should be called when the underlying header layout is changed.
 func (c *Composer) updateGrid() {
-	grid := ui.NewGrid().Columns([]ui.GridSpec{
-		{Strategy: ui.SIZE_WEIGHT, Size: ui.Const(1)},
-	})
+	grid := ui.NewGrid()
+	var mainCol int = 0
+	if c.centeredLayoutWidth != 0 {
+		grid = ui.NewGrid().Columns([]ui.GridSpec{
+			{Strategy: ui.SIZE_WEIGHT, Size: ui.Const(1)},
+			{Strategy: ui.SIZE_EXACT, Size: ui.Const(c.centeredLayoutWidth)},
+			{Strategy: ui.SIZE_WEIGHT, Size: ui.Const(1)},
+		})
+		mainCol = 1
+	} else {
+		grid = ui.NewGrid().Columns([]ui.GridSpec{
+			{Strategy: ui.SIZE_WEIGHT, Size: ui.Const(1)},
+		})
+	}
 
 	if c.editHeaders && c.review == nil {
 		grid.Rows([]ui.GridSpec{
@@ -1512,7 +1529,7 @@ func (c *Composer) updateGrid() {
 			{Strategy: ui.SIZE_WEIGHT, Size: ui.Const(1)},
 		})
 		if c.editor != nil {
-			grid.AddChild(c.editor).At(0, 0)
+			grid.AddChild(c.editor).At(0, mainCol)
 		}
 		c.grid.Store(grid)
 		return
@@ -1541,13 +1558,17 @@ func (c *Composer) updateGrid() {
 
 	borderStyle := c.acct.UiConfig().GetStyle(config.STYLE_BORDER)
 	borderChar := c.acct.UiConfig().BorderCharHorizontal
-	grid.AddChild(heditors).At(0, 0)
-	grid.AddChild(c.crypto).At(1, 0)
-	grid.AddChild(ui.NewFill(borderChar, borderStyle)).At(2, 0)
+	grid.AddChild(heditors).At(0, mainCol)
+	grid.AddChild(c.crypto).At(1, mainCol)
+	grid.AddChild(ui.NewFill(borderChar, borderStyle)).At(2, mainCol)
+	if mainCol == 1 {
+		grid.AddChild(ui.NewFill(borderChar, borderStyle)).At(2, 0)
+		grid.AddChild(ui.NewFill(borderChar, borderStyle)).At(2, 2)
+	}
 	if c.review != nil {
-		grid.AddChild(c.review).At(3, 0)
+		grid.AddChild(c.review).At(3, mainCol)
 	} else if c.editor != nil {
-		grid.AddChild(c.editor).At(3, 0)
+		grid.AddChild(c.editor).At(3, mainCol)
 	}
 	c.heditors.Store(heditors)
 	c.grid.Store(grid)
@@ -1774,54 +1795,65 @@ func newReviewMessage(composer *Composer, err error) *reviewMessage {
 	// make the last element fill remaining space
 	spec = append(spec, ui.GridSpec{Strategy: ui.SIZE_WEIGHT, Size: ui.Const(1)})
 
-	grid := ui.NewGrid().Rows(spec).Columns([]ui.GridSpec{
-		{Strategy: ui.SIZE_WEIGHT, Size: ui.Const(1)},
-	})
+	var mainCol int = 0
+	grid := ui.NewGrid()
+	if composer.centeredLayoutWidth != 0 {
+		grid = ui.NewGrid().Rows(spec).Columns([]ui.GridSpec{
+			{Strategy: ui.SIZE_WEIGHT, Size: ui.Const(1)},
+			{Strategy: ui.SIZE_EXACT, Size: ui.Const(composer.centeredLayoutWidth)},
+			{Strategy: ui.SIZE_WEIGHT, Size: ui.Const(1)},
+		})
+		mainCol = 1
+	} else {
+		grid = ui.NewGrid().Rows(spec).Columns([]ui.GridSpec{
+			{Strategy: ui.SIZE_WEIGHT, Size: ui.Const(1)},
+		})
+	}
 
 	uiConfig := composer.acct.UiConfig()
 
 	if err != nil {
 		grid.AddChild(ui.NewText(err.Error(), uiConfig.GetStyle(config.STYLE_ERROR)))
 		grid.AddChild(ui.NewText("Press [q] to close this tab.",
-			uiConfig.GetStyle(config.STYLE_DEFAULT))).At(1, 0)
+			uiConfig.GetStyle(config.STYLE_DEFAULT))).At(1, mainCol)
 	} else {
 		grid.AddChild(ui.NewText("Send this email?",
-			uiConfig.GetStyle(config.STYLE_TITLE))).At(0, 0)
+			uiConfig.GetStyle(config.STYLE_TITLE))).At(0, mainCol)
 		i := 1
 		for _, action := range actions {
 			grid.AddChild(ui.NewText(action,
-				uiConfig.GetStyle(config.STYLE_DEFAULT))).At(i, 0)
+				uiConfig.GetStyle(config.STYLE_DEFAULT))).At(i, mainCol)
 			i += 1
 		}
 		grid.AddChild(ui.NewText("Attachments:",
-			uiConfig.GetStyle(config.STYLE_TITLE))).At(i, 0)
+			uiConfig.GetStyle(config.STYLE_TITLE))).At(i, mainCol)
 		i += 1
 		if len(composer.attachments) == 0 {
 			grid.AddChild(ui.NewText("(none)",
-				uiConfig.GetStyle(config.STYLE_DEFAULT))).At(i, 0)
+				uiConfig.GetStyle(config.STYLE_DEFAULT))).At(i, mainCol)
 			i += 1
 		} else {
 			for _, a := range composer.attachments {
 				grid.AddChild(ui.NewText(a.Name(), uiConfig.GetStyle(config.STYLE_DEFAULT))).
-					At(i, 0)
+					At(i, mainCol)
 				i += 1
 			}
 		}
 		if len(composer.textParts) > 0 {
 			grid.AddChild(ui.NewText("Parts:",
-				uiConfig.GetStyle(config.STYLE_TITLE))).At(i, 0)
+				uiConfig.GetStyle(config.STYLE_TITLE))).At(i, mainCol)
 			i += 1
-			grid.AddChild(ui.NewText("text/plain", uiConfig.GetStyle(config.STYLE_DEFAULT))).At(i, 0)
+			grid.AddChild(ui.NewText("text/plain", uiConfig.GetStyle(config.STYLE_DEFAULT))).At(i, mainCol)
 			i += 1
 			for _, p := range composer.textParts {
 				err := composer.updateMultipart(p)
 				if err != nil {
 					msg := fmt.Sprintf("%s error: %s", p.MimeType, err)
 					grid.AddChild(ui.NewText(msg,
-						uiConfig.GetStyle(config.STYLE_ERROR))).At(i, 0)
+						uiConfig.GetStyle(config.STYLE_ERROR))).At(i, mainCol)
 				} else {
 					grid.AddChild(ui.NewText(p.MimeType,
-						uiConfig.GetStyle(config.STYLE_DEFAULT))).At(i, 0)
+						uiConfig.GetStyle(config.STYLE_DEFAULT))).At(i, mainCol)
 				}
 				i += 1
 			}

app/msgviewer.go
@@ -123,9 +123,20 @@ func NewMessageViewer(
 		{Strategy: ui.SIZE_WEIGHT, Size: ui.Const(1)},
 	}...)
 
-	grid := ui.NewGrid().Rows(rows).Columns([]ui.GridSpec{
-		{Strategy: ui.SIZE_WEIGHT, Size: ui.Const(1)},
-	})
+	grid := ui.NewGrid()
+	var mainCol int = 0
+	if acct.UiConfig().CenteredLayoutWidth != 0 {
+		grid = ui.NewGrid().Rows(rows).Columns([]ui.GridSpec{
+			{Strategy: ui.SIZE_WEIGHT, Size: ui.Const(1)},
+			{Strategy: ui.SIZE_EXACT, Size: ui.Const(acct.UiConfig().CenteredLayoutWidth)},
+			{Strategy: ui.SIZE_WEIGHT, Size: ui.Const(1)},
+		})
+		mainCol = 1
+	} else {
+		grid = ui.NewGrid().Rows(rows).Columns([]ui.GridSpec{
+			{Strategy: ui.SIZE_WEIGHT, Size: ui.Const(1)},
+		})
+	}
 
 	switcher := &PartSwitcher{}
 	err := createSwitcher(acct, switcher, msg)
@@ -136,14 +147,22 @@ func NewMessageViewer(
 	borderStyle := acct.UiConfig().GetStyle(config.STYLE_BORDER)
 	borderChar := acct.UiConfig().BorderCharHorizontal
 
-	grid.AddChild(header).At(0, 0)
+	grid.AddChild(header).At(0, mainCol)
 	if msg.MessageDetails() != nil || acct.UiConfig().IconUnencrypted != "" {
-		grid.AddChild(NewPGPInfo(msg.MessageDetails(), acct.UiConfig())).At(1, 0)
-		grid.AddChild(ui.NewFill(borderChar, borderStyle)).At(2, 0)
-		grid.AddChild(switcher).At(3, 0)
+		grid.AddChild(NewPGPInfo(msg.MessageDetails(), acct.UiConfig())).At(1, mainCol)
+		grid.AddChild(ui.NewFill(borderChar, borderStyle)).At(2, mainCol)
+		if mainCol == 1 {
+			grid.AddChild(ui.NewFill(borderChar, borderStyle)).At(2, 0)
+			grid.AddChild(ui.NewFill(borderChar, borderStyle)).At(2, 2)
+		}
+		grid.AddChild(switcher).At(3, mainCol)
 	} else {
-		grid.AddChild(ui.NewFill(borderChar, borderStyle)).At(1, 0)
-		grid.AddChild(switcher).At(2, 0)
+		grid.AddChild(ui.NewFill(borderChar, borderStyle)).At(1, mainCol)
+		if mainCol == 1 {
+			grid.AddChild(ui.NewFill(borderChar, borderStyle)).At(1, 0)
+			grid.AddChild(ui.NewFill(borderChar, borderStyle)).At(1, 2)
+		}
+		grid.AddChild(switcher).At(2, mainCol)
 	}
 
 	mv := &MessageViewer{

config/aerc.conf
@@ -571,6 +571,12 @@
 # Default: ""
 #thread-prefix-orphan-reverse = ""
 
+#
+# Center the editor and the message viewer on screen with a fixed width.
+#
+# Default: 0
+#centered-layout-width = 0
+
 [statusline]
 #
 # Describes the format for the status line. This is a comma separated list of

config/ui.go
@@ -83,6 +83,7 @@ type UIConfig struct {
 	DialogHeight                  int           `ini:"dialog-height" default:"50" parse:"ParseDialogDimensions"`
 	StyleSetDirs                  []string      `ini:"stylesets-dirs" delim:":"`
 	StyleSetName                  string        `ini:"styleset-name" default:"default"`
+	CenteredLayoutWidth           int           `ini:"centered-layout-width" default:"0"`
 
 	// customize border appearance
 	BorderCharVertical   rune `ini:"border-char-vertical" default:"│" type:"rune"`

doc/aerc-config.5.scd
@@ -703,7 +703,6 @@ following configurable prefix parts:
 
 	Default: _""_
 
-
 Default settings (mutt-style):
 
 	```
@@ -748,6 +747,16 @@ thread-prefix-dummy = "┬"
 thread-prefix-lone = " "
 thread-prefix-last-sibling = "╰"
 ```
+
+*centered-layout-width* = _<width>_
+	Center the editor and the message viewer on screen by setting a fixed
+	width for them.
+
+	_<width>_ is a positive integer representing the width (in terminal cells)
+	of the editor and message viewer windows.
+
+	Default: _""_
+
 
 ## CONTEXTUAL UI CONFIGURATION
 

commit bed449e3434a86b954c74bc60abafa77b56d7130

ui: center layout only if it fits
If centered-layout-width is bigger than the actual dynamic width of the
window, the message viewer, the composer and the review screen are
currently not shown at all. Properly show all of them also when the
window width is small.

Fixes: 741f93d991bb ("ui: allow centering the message viewer and composer")
Changelog-fixed: When centered-layout-width is on and the window
 is too small, the msgviewer, the composer and the reviewer are now
 properly shown.
Reported-by: Antonin Godard <antonin@godard.cc>
Signed-off-by: inwit <inwit@sindominio.net>
Tested-by: Antonin Godard <antonin@godard.cc>
Acked-by: Robin Jarry <robin@jarry.cc>

app/compose.go
@@ -81,7 +81,7 @@ type Composer struct {
 }
 
 func (o *withCenteredGrid) centerWidth() int {
-	if o.centeredLayoutWidth > 0 {
+	if o.centeredLayoutWidth > 0 && o.width >= o.centeredLayoutWidth {
 		return o.centeredLayoutWidth
 	}
 	return o.width