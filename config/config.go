eca65062b8ab5484d2147c06d0be6115ad4f30e0
Tristan Partin <tristan@partin.io>
2026-07-31 17:40:46 +0000

    config: pull default directory code out of config.go
    This will make future changes easier to review.
    
    Signed-off-by: Tristan Partin <tristan@partin.io>
    Acked-by: Robin Jarry <robin@jarry.cc>

--- config/config.go
+++ config/config.go
@@ -12,51 +12,6 @@ import (
 	"github.com/go-ini/ini"
 )
 
-// Set at build time
-var (
-	shareDir   string
-	libexecDir string
-)
-
-func buildDefaultDirs() []string {
-	var defaultDirs []string
-
-	prefixes := []string{
-		xdg.ConfigPath(),
-		"~/.local/libexec",
-		xdg.DataPath(),
-	}
-
-	// Add XDG_CONFIG_HOME and XDG_DATA_HOME
-	for _, v := range prefixes {
-		if v != "" {
-			defaultDirs = append(defaultDirs, xdg.ExpandHome(v, "aerc"))
-		}
-	}
-
-	// Trim null chars inserted post-build by systems like Conda
-	shareDir := strings.TrimRight(shareDir, "\x00")
-	libexecDir := strings.TrimRight(libexecDir, "\x00")
-
-	// Add custom buildtime dirs
-	if libexecDir != "" && libexecDir != "/usr/local/libexec/aerc" {
-		defaultDirs = append(defaultDirs, xdg.ExpandHome(libexecDir))
-	}
-	if shareDir != "" && shareDir != "/usr/local/share/aerc" {
-		defaultDirs = append(defaultDirs, xdg.ExpandHome(shareDir))
-	}
-
-	// Add fixed fallback locations
-	defaultDirs = append(defaultDirs, "/usr/local/libexec/aerc")
-	defaultDirs = append(defaultDirs, "/usr/local/share/aerc")
-	defaultDirs = append(defaultDirs, "/usr/libexec/aerc")
-	defaultDirs = append(defaultDirs, "/usr/share/aerc")
-
-	return defaultDirs
-}
-
-var SearchDirs = buildDefaultDirs()
-
 func installTemplate(root, name string) error {
 	var err error
 	if _, err = os.Stat(root); os.IsNotExist(err) {
---
+++ config/paths.go
@@ -0,0 +1,52 @@
+package config
+
+import (
+	"strings"
+
+	"git.sr.ht/~rjarry/aerc/lib/xdg"
+)
+
+// Set at build time
+var (
+	shareDir   string
+	libexecDir string
+)
+
+func buildDefaultDirs() []string {
+	var defaultDirs []string
+
+	prefixes := []string{
+		xdg.ConfigPath(),
+		"~/.local/libexec",
+		xdg.DataPath(),
+	}
+
+	// Add XDG_CONFIG_HOME and XDG_DATA_HOME
+	for _, v := range prefixes {
+		if v != "" {
+			defaultDirs = append(defaultDirs, xdg.ExpandHome(v, "aerc"))
+		}
+	}
+
+	// Trim null chars inserted post-build by systems like Conda
+	shareDir := strings.TrimRight(shareDir, "\x00")
+	libexecDir := strings.TrimRight(libexecDir, "\x00")
+
+	// Add custom buildtime dirs
+	if libexecDir != "" && libexecDir != "/usr/local/libexec/aerc" {
+		defaultDirs = append(defaultDirs, xdg.ExpandHome(libexecDir))
+	}
+	if shareDir != "" && shareDir != "/usr/local/share/aerc" {
+		defaultDirs = append(defaultDirs, xdg.ExpandHome(shareDir))
+	}
+
+	// Add fixed fallback locations
+	defaultDirs = append(defaultDirs, "/usr/local/libexec/aerc")
+	defaultDirs = append(defaultDirs, "/usr/local/share/aerc")
+	defaultDirs = append(defaultDirs, "/usr/libexec/aerc")
+	defaultDirs = append(defaultDirs, "/usr/share/aerc")
+
+	return defaultDirs
+}
+
+var SearchDirs = buildDefaultDirs()
