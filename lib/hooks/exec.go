ea8fd6efb8d73ae2e7c08f1d84d554c9a08878ae
Tristan Partin <tristan@partin.io>
2026-07-31 17:40:49 +0000

    hooks: add hooks subdirectories to PATH on execution
    When executing filters, we add the various filters subdirectories of our
    search directories. However, we were not doing the same for hooks. By
    searching for hooks in the subdirectories, users can keep their `PATH`
    a bit cleaner and hook can be looked up by name without the user needing
    to do their own `PATH` manipulation.
    
    Changelog-added: Hooks are now also searched in
     `${XDG_CONFIG_HOME:-~/.config}/aerc/hooks`,
     `~/.local/libexec/aerc/hooks`,
     `${XDG_DATA_HOME:-~/.local/share}/aerc/hooks`,
     `$PREFIX/libexec/aerc/hooks`, `$PREFIX/share/aerc/hooks`,
     `/usr/libexec/aerc/hooks`, `/usr/share/aerc/hooks`.
    Co-authored-by: Robin Jarry <robin@rjarry.cc>
    Signed-off-by: Tristan Partin <tristan@partin.io>
    Acked-by: Robin Jarry <robin@jarry.cc>

--- lib/hooks/exec.go
+++ lib/hooks/exec.go
@@ -2,9 +2,12 @@ package hooks
 
 import (
 	"bytes"
+	"fmt"
 	"os"
 	"os/exec"
+	"strings"
 
+	"git.sr.ht/~rjarry/aerc/config"
 	"git.sr.ht/~rjarry/aerc/lib/log"
 )
 
@@ -17,11 +20,21 @@ func RunHook(h HookType) error {
 	log.Debugf("hooks: running %T command %q (env %v)", h, cmd, env)
 
 	proc := exec.Command("sh", "-c", cmd)
+
 	var outb, errb bytes.Buffer
 	proc.Stdout = &outb
 	proc.Stderr = &errb
+
+	// add hook environment variables
 	proc.Env = os.Environ()
 	proc.Env = append(proc.Env, env...)
+
+	// append to PATH to find hooks
+	paths := strings.Split(os.Getenv("PATH"), string(os.PathListSeparator))
+	paths = append(config.ResourceDirs("hooks"), paths...)
+	path := strings.Join(paths, string(os.PathListSeparator))
+	proc.Env = append(proc.Env, fmt.Sprintf("PATH=%s", path))
+
 	err := proc.Run()
 	log.Tracef("hooks: %q stdout: %s", cmd, outb.String())
 	if err != nil {
