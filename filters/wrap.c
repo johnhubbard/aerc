johnhubbard/aerc

commit d3288e6359744ad0383d08270aea74f4497c974d

wrap: use nl_langinfo to get locale codeset
Using more and more complicated constructs comparing locale strings
doesn't lead to anywhere. Even with the current two steps long
comparison the program is wrong on systems which use "utf8" in their
locale names (e.g., my openSUSE/Tumbleweed). It is better to use proper
locale functions to get canonical name of the current codeset.

Link: https://lists.sr.ht/~rjarry/aerc-devel/%3CD1L7746XIJ3Z.3SN11SOVO175Q@cepl.eu%3E
Signed-off-by: Matěj Cepl <mcepl@cepl.eu>
Acked-by: Robin Jarry <robin@jarry.cc>

filters/wrap.c
@@ -1,9 +1,10 @@
 /* SPDX-License-Identifier: MIT */
 /* Copyright (c) 2023 Robin Jarry */
 
-#define _XOPEN_SOURCE
+#define _XOPEN_SOURCE 700
 #include <errno.h>
 #include <getopt.h>
+#include <langinfo.h>
 #include <locale.h>
 #include <regex.h>
 #include <stdbool.h>
@@ -446,7 +447,10 @@ static int set_stdio_encoding(void)
 	}
 
 	/* aerc will always send UTF-8 text, ensure that we read that properly */
-	if (!strstr(locale, "UTF-8") && !strstr(locale, "utf-8")) {
+	locale_t loc = newlocale(LC_ALL_MASK, locale, NULL);
+	char *codeset = nl_langinfo_l(CODESET, loc);
+	freelocale(loc);
+	if (!strstr(codeset, "UTF-8")) {
 		fprintf(stderr, "error: locale '%s' is not UTF-8\n", locale);
 		return 1;
 	}

commit c44d033bb25e85d9db0740bd7037ba93e45daace

wrap: fix use-after-free on locale codeset check
The codeset pointer returned by nl_langinfo_l() may be invalidated by
freelocale(). Evaluate the result before freeing the locale object.

Fixes: d3288e635974 ("wrap: use nl_langinfo to get locale codeset")
Signed-off-by: Robin Jarry <robin@jarry.cc>
Reviewed-by: Tristan Partin <tristan@partin.io>

filters/wrap.c
@@ -514,9 +514,14 @@ static int set_stdio_encoding(void)
 
 	/* aerc will always send UTF-8 text, ensure that we read that properly */
 	locale_t loc = newlocale(LC_ALL_MASK, locale, NULL);
+	if (!loc) {
+		fprintf(stderr, "error: failed to create locale '%s'\n", locale);
+		return 1;
+	}
 	char *codeset = nl_langinfo_l(CODESET, loc);
+	bool utf8 = strstr(codeset, "UTF-8") != NULL;
 	freelocale(loc);
-	if (!strstr(codeset, "UTF-8")) {
+	if (!utf8) {
 		fprintf(stderr, "error: locale '%s' is not UTF-8\n", locale);
 		return 1;
 	}